e04439a9e828fd613105d81589b16fb6c3d60037
Philip Edekobi <philipedekobi@gmail.com>
2024-02-21 15:03:34 +0100

    match test

--- searcher/query_test.go
+++ searcher/query_test.go
@@ -34,3 +34,27 @@ func TestParseFilter(t *testing.T) {
 
 	require.Equal(t, expected, ans)
 }
+
+func TestMatch(t *testing.T) {
+	docs := []map[string]any{
+		{
+			"name": "andy",
+			"age":  "17",
+		},
+		{
+			"class": map[string]any{
+				"upper": "12",
+			},
+		},
+	}
+
+	q, err := parseFilter("age:>16")
+	require.Nil(t, err)
+
+	q2, err := parseFilter("class.upper:>9")
+	require.Nil(t, err)
+
+	require.Equal(t, true, q.Match(docs[0]))
+	require.Equal(t, true, q2.Match(docs[1]))
+	require.Equal(t, false, q2.Match(docs[0]))
+}
